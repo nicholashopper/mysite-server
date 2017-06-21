095a346008002e136502f86de3dfbc211fc53153
Nicholas <nicholas.l.hopper@gmail.com>
2017-06-21 18:37:40 -0500

    Fixed some frontend configs

--- run-dev.sh
+++ run-dev.sh
@@ -1,10 +1,1 @@
-cd app
-git stash save --keep-index
-git stash drop
-git pull
-cd ../frontend
-git stash save --keep-index
-git stash drop
-git pull
-cd ../
 docker-compose build && docker-compose up
